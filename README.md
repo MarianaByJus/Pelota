909d2d6770207925de2dbdf271ff450580439591
MarianaByJus <86327356+MarianaByJus@users.noreply.github.com>
2024-11-25 18:12:12 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Pelota
+# C35_Pelota con movimiento sincrónico
+Actividad del alumno 1
---
+++ sketch.js
@@ -0,0 +1,24 @@
+var hypnoticBall, database;
+var position;
+
+
+function setup(){
+
+}
+
+function draw(){
+  background("white");
+  
+   
+    drawSprites();
+  
+}
+
+function writePosition(x,y){
+ 
+}
+
+function readPosition(data){
+
+}
+
